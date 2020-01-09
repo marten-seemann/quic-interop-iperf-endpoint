5fc341fd2de50db6abfa2bad1a928c65b4e15995
Marten Seemann <martenseemann@gmail.com>
2020-01-09 14:16:39 +0700

    initial commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./setup.sh
+./wait-for-it.sh sim:57832 -s -t 10
+
+if [ "$ROLE" == "server" ]; then
+  ./wait-for-it.sh client:5001 -s -t 10
+  iperf -c client -t 3600 -e -i 1 -Z $IPERF_CONGESTION
+else
+  iperf -s -m 
+fi
