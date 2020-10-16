44d5fa65401f5b3d6cb3943dcd24ad670dab5e08
Marten Seemann <martenseemann@gmail.com>
2020-10-16 20:18:47 +0700

    use an environment variable for the client's hostname

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 ./wait-for-it.sh sim:57832 -s -t 10
 
 if [ "$ROLE" == "server" ]; then
-  ./wait-for-it.sh client:5001 -s -t 10
-  iperf -c client -t 3600 -e -i 1 -Z $IPERF_CONGESTION
+  ./wait-for-it.sh $CLIENT:5001 -s -t 10
+  iperf -c $CLIENT -t 3600 -e -i 1 -Z $IPERF_CONGESTION
 else
   iperf -s -m -i 1
 fi
